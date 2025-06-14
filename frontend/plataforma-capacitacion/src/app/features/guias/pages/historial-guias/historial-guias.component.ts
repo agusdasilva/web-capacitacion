b054996f1dba90e7e00de666b0c2f75c59c7240a
agusdasilva <agusdasilva2004@gmail.com>
2025-06-14 12:38:19 -0300

    feat: implement guias components

--- frontend/plataforma-capacitacion/src/app/features/guias/pages/historial-guias/historial-guias.component.ts
+++ frontend/plataforma-capacitacion/src/app/features/guias/pages/historial-guias/historial-guias.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GuiasService } from '../../services/guias.service';
 
 @Component({
   selector: 'app-historial-guias',
-  imports: [],
   templateUrl: './historial-guias.component.html',
-  styleUrl: './historial-guias.component.css'
+  styleUrl: './historial-guias.component.css',
+  standalone: false
 })
-export class HistorialGuiasComponent {
+export class HistorialGuiasComponent implements OnInit {
+  vistas: any[] = [];
+  cargando = true;
 
+  constructor(private guiasSvc: GuiasService) {}
+
+  ngOnInit(): void {
+    this.guiasSvc.historial().subscribe({
+      next: data => {
+        this.vistas = data;
+        this.cargando = false;
+      },
+      error: () => {
+        this.cargando = false;
+      }
+    });
+  }
 }
--- frontend/plataforma-capacitacion/src/app/features/guias/pages/listado-guias/listado-guias.component.ts
+++ frontend/plataforma-capacitacion/src/app/features/guias/pages/listado-guias/listado-guias.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GuiasService } from '../../services/guias.service';
+import { Guia } from '../../../../core/models';
 
 @Component({
   selector: 'app-listado-guias',
-  imports: [],
   templateUrl: './listado-guias.component.html',
-  styleUrl: './listado-guias.component.css'
+  styleUrl: './listado-guias.component.css',
+  standalone: false
 })
-export class ListadoGuiasComponent {
+export class ListadoGuiasComponent implements OnInit {
+  guias: Guia[] = [];
+  cargando = true;
 
+  constructor(private guiasSvc: GuiasService) {}
+
+  ngOnInit(): void {
+    this.guiasSvc.listar().subscribe({
+      next: data => {
+        this.guias = data;
+        this.cargando = false;
+      },
+      error: () => {
+        this.cargando = false;
+      }
+    });
+  }
+
+  abrir(guia: Guia) {
+    this.guiasSvc.registrarVista(guia.id).subscribe();
+    window.open(guia.archivo, '_blank');
+  }
 }
